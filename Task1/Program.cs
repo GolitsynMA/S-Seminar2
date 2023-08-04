3855453826fa113a3e3a6ccca1e02514bfda8086
«Misha» <golitsynma@example.com>
2023-08-04 22:02:34 +0300

    add task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+
+int a = number / 10;
+int b = number % 10;
+
+Console.WriteLine(number);
+
+if (a > b) 
+{
+    Console.WriteLine("Максимальное число a = " + a);
+}
+else if (b > a)
+{
+    Console.WriteLine("Максимальное {1} число b = {0} ", b, a);
+}
+else {
+    Console.WriteLine($"Все числа равны {a} = {b}");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Random rand = new Random();
+int randomNumber = rand.Next(100, 1000);
+Console.WriteLine(randomNumber);
+
+
+int a = randomNumber / 100;
+int b = randomNumber % 10;
+Console.WriteLine($"{a}{b}");
